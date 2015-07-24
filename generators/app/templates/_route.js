e7689c1c07c64f86b9cead342080995faf026341
Abderrezak Mohamed Djadoun <ma.djadoun@gpcdz.com>
2015-07-24 18:39:20 +0100

    added comments to the router

--- generators/app/templates/_route.js
+++ generators/app/templates/_route.js
@@ -1,7 +1,15 @@
 "use strict";
 
 angular.module('<%= appname %>')
+/*
+<%= moduleName %>: description of the state 
 
+NOTE: if the state has any params they should also be included and described individually
+exemple: state/:param1/:param3
+ 
+param1: description 1
+param2: description 2
+ */
 .config(function($stateProvider) {
   $stateProvider
 
